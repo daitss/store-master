8399aa2a4937c71e80f6b7a9a7e7a2e40b3af28d
Randy Fischer <fischer@sacred.net>
2011-02-23 15:19:23 -0500

    bad ordering bug in storage package listings; timing info added to reports

---
+++ tools/irb-tarchive.rb
@@ -0,0 +1,20 @@
+# -*- mode: ruby; -*-
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'lib')
+
+require 'daitss/model'            # DaitssModel
+require 'store-master'      # StoreMasterModel
+
+DataMapper::Logger.new(STDERR, :debug)
+
+# testing db:
+#
+# Daitss.setup_db('/opt/fda/etc/db.yml', 'store_daitss_postgres') 
+# Daitss.create_tables
+
+DaitssModel.setup_db('/opt/fda/etc/db.yml', 'ps_daitss_2')
+StoreMasterModel.setup_db('/opt/fda/etc/db.yml', 'ps_store_master')
+
+@sm = Streams::StoreMasterPackageStream.new
+
+
