d616523aac56b523d9a13ca716626c639ac03088
Randy Fischer <fischer@sacred.net>
2011-08-04 12:45:17 -0400

    bump middle version number

--- lib/store-master.rb
+++ lib/store-master.rb
@@ -49,7 +49,7 @@ module StoreMaster
 
   REVISION = get_capistrano_git_revision()
   RELEASE  = get_capistrano_release()
-  VERSION  = '1.2.0'
+  VERSION  = '1.3.0'
   NAME     = 'Store Master Service'
 
   def self.version
