790602495fd9877d09cf363d6207cfc282b323b4
Manny Rodriguez <manny@bianchi.fcla.edu>
2011-12-21 11:56:41 -0500

    model for log table to store an audit trail for releases

---
+++ lib/storage-master/model/log.rb
@@ -0,0 +1,17 @@
+require 'storage-master/exceptions'
+require 'storage-master/model'
+
+module StorageMasterModel
+
+  class Log
+    include DataMapper::Resource
+    include StorageMaster
+
+    property  :id,         Serial,   :min => 1
+    property  :timestamp,  DateTime, :index => true, :default => lambda { |resource, property| DateTime.now }
+    property  :action,     String,   :length => 255
+    property  :user,       String,   :length => 255
+    property  :note,       Text
+  end
+end
+
