ed6497e21c4dd90fc55cccc5558aaa84a8a896c6
Carol Chou <cchou@ufl.edu>
2012-01-31 12:49:33 -0500

    add url column

--- lib/storage-master/model/log.rb
+++ lib/storage-master/model/log.rb
@@ -11,6 +11,7 @@ module StorageMasterModel
     property  :timestamp,  DateTime, :index => true, :default => lambda { |resource, property| DateTime.now }
     property  :action,     String,   :length => 255
     property  :user,       String,   :length => 255
+    property  :url,        String,   :length => 225
     property  :note,       Text
   end
 end
