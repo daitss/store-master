daitss/store-master

commit 62cfe2a76efde90d94d19e02b920140ca36a576e

initial commit for ruby 1.9.3

Gemfile
@@ -9,6 +9,7 @@ gem 'libxml-ruby', :require => 'libxml'
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'dm-mysql-adapter'
 
 gem 'builder'
 gem 'log4r'
@@ -21,12 +22,12 @@ gem 'thin'
 # development
 
 gem 'rake'
-gem 'rcov'
+gem 'simplecov'
 gem 'cucumber'
 gem 'rspec'
  
 group :test do
-  gem 'ruby-debug'
+  gem 'debugger'
 end
 
 

app.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require 'storage-master'
 require 'app/helpers'
 require 'app/package-reports'
@@ -33,7 +34,6 @@ end
 
 
 configure do
-  $KCODE = 'UTF8'
 
   config = get_config
 

config.ru
@@ -2,8 +2,13 @@
 
 require 'rubygems'
 require 'bundler/setup'
-
-$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
+p "ruby version=#{RUBY_VERSION}"
+require 'debugger'
+#$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
+#$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), './'))
+#$LOAD_PATH.unshift File.expand_path(File.join('../'))
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__))
+$LOAD_PATH.each_with_index {|z,i| p "i=#{i}  z=#{z}"}
 
 require 'sinatra'
 require 'app'

.ruby-version
@@ -1,0 +1,1 @@
+1.9.3

spec/db.yml
@@ -1,7 +1,7 @@
 # The databases and access that must exist prior to doing the db-based spec tests:
 #
 store_master_mysql:     { vendor: mysql,    hostname: localhost, database: store_master_spec_test, username: root, password: }
-store_master_postgres:  { vendor: postgres, hostname: localhost, database: store_master_spec_test, username: fischer, password: }
+store_master_postgres:  { vendor: postgres, hostname: localhost, database: store_master_spec_test, username: fischer, password: topsecret }
 
 store_daitss_mysql:     { vendor: mysql,    hostname: localhost, database: store_master_daitss_spec_test, username: root, password: }
-store_daitss_postgres:  { vendor: postgres, hostname: localhost, database: store_master_daitss_spec_test, username: fischer, password: }
+store_daitss_postgres:  { vendor: postgres, hostname: localhost, database: store_master_daitss_spec_test, username: fischer, password: topsecret }

spec/package_spec.rb
@@ -114,7 +114,7 @@ def index_of_pool_that_best_matches_url(pools, url)
 end
 
 
-@@all_package_names = []
+StorageMasterModel.class_variable_set(:@@all_package_names,[])
 
 def nimby
   pending "No active pools are available; can't run this test" unless test_pools
@@ -127,9 +127,9 @@ end
 describe Package do
 
   before(:all) do
-    @@pools = []
+    StorageMasterModel.class_variable_set(:@@pools,[])
     datamapper_setup
-    test_pools.each { |pool| p = Pool.add(pool); @@pools.push p }
+    test_pools.each { |pool| p = Pool.add(pool); StorageMasterModel.class_variable_get(:@@pools).push p }
   end
 
   it "should let us determine that a package doesn't exist" do
@@ -223,16 +223,15 @@ describe Package do
   it "should list all of the package names we've stored" do    
     names = []
     Package.list { |pkg| names.push pkg.name }
-    (names - @@all_package_names).should == []
-    (@@all_package_names - names).should == []
+    (names - StorageMasterModel.class_variable_get(:@@all_package_names)).should == []
+    (StorageMasterModel.class_variable_get(:@@all_package_names) - names).should == []
   end
-
 
   it "should allow us to delete packages" do
 
     # get all the locations for all the names
 
-    @@all_package_names.each do |name|
+    StorageMasterModel.class_variable_get(:@@all_package_names).each do |name|
       Package.exists?(name).should == true
       pkg = Package.lookup(name)
       pkg.nil?.should == false
@@ -240,5 +239,4 @@ describe Package do
       Package.exists?(name).should == false
     end
   end
-
 end

commit 284cd9c555d4717994ddc05c92c0c8bb224b262d

fix the problems introduced by commit 62cfe2a76efde90d94d19e02b920140ca36a576e

Gemfile
@@ -9,7 +9,6 @@ gem 'libxml-ruby', :require => 'libxml'
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
-gem 'dm-mysql-adapter'
 
 gem 'builder'
 gem 'log4r'

app.rb
@@ -46,29 +46,29 @@ configure do
 
   set :required_pools, config.required_pools
 
-  Logger.setup('StorageMaster', ENV['VIRTUAL_HOSTNAME'])
+  Datyl::Logger.setup('StorageMaster', ENV['VIRTUAL_HOSTNAME'])
 
-  Logger.facility = config.log_syslog_facility  if config.log_syslog_facility
-  Logger.filename = config.log_filename         if config.log_filename
-  Logger.stderr     unless (config.log_filename or config.log_syslog_facility)
+  Datyl::Logger.facility = config.log_syslog_facility  if config.log_syslog_facility
+  Datyl::Logger.filename = config.log_filename         if config.log_filename
+  Datyl::Logger.stderr     unless (config.log_filename or config.log_syslog_facility)
 
-  use Rack::CommonLogger, Logger.new(:info, 'Rack:')  # Bend CommonLogger to our logging system
+  use Rack::CommonLogger, Datyl::Logger.new(:info, 'Rack:')  # Bend CommonLogger to our logging system
 
-  Logger.info "Starting #{StorageMaster.version.name}"
+  Datyl::Logger.info "Starting #{StorageMaster.version.name}"
 
   case settings.required_pools
   when 0
-    Logger.info "No silo pools are required: this storage master will act as a testing-only stub server and not actually store to any silo-pools."
+    Datyl::Logger.info "No silo pools are required: this storage master will act as a testing-only stub server and not actually store to any silo-pools."
   when 1
-    Logger.info "Requiring one silo pool for storage."
+    Datyl::Logger.info "Requiring one silo pool for storage."
   else
-    Logger.info "Requiring #{settings.required_pools} silo pools for storage."
+    Datyl::Logger.info "Requiring #{settings.required_pools} silo pools for storage."
   end
 
-  Logger.info "Using #{ENV['TMPDIR'] || 'system default'} for temp directory"
-  Logger.info "Using database #{StoreUtils.safen_connection_string(config.storage_master_db)}"
+  Datyl::Logger.info "Using #{ENV['TMPDIR'] || 'system default'} for temp directory"
+  Datyl::Logger.info "Using database #{StoreUtils.safen_connection_string(config.storage_master_db)}"
 
-  DataMapper::Logger.new(Logger.new(:info, 'DataMapper:'), :debug) if config.log_database_queries
+  DataMapper::Logger.new(Datyl::Logger.new(:info, 'DataMapper:'), :debug) if config.log_database_queries
 
   StorageMaster.setup_databases(config.storage_master_db)
 end

config.ru
@@ -2,15 +2,10 @@
 
 require 'rubygems'
 require 'bundler/setup'
-p "ruby version=#{RUBY_VERSION}"
-require 'debugger'
-#$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
-#$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), './'))
-#$LOAD_PATH.unshift File.expand_path(File.join('../'))
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__))
-$LOAD_PATH.each_with_index {|z,i| p "i=#{i}  z=#{z}"}
+
+$LOAD_PATH.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'sinatra'
-require 'app'
+require './app'
 
 run Sinatra::Application